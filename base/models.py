89378fbf24c317f393d863871bfcafde5e7b11aa
m-zoubir <lm_zoubir@esi.dz>
2025-03-27 00:20:38 +0100

    initial commit

---
+++ base/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+
+
+class Component(models.Model):
+    numero_serie = models.CharField(max_length=200)
+    numero_serie_eq = models.CharField(max_length=200)
+    numero_inventaire_eq = models.CharField(max_length=200)
+    designation = models.TextField()
+    observation = models.TextField(null=True, blank=True)
+    model_reference = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.designation
---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def getComponents(request):
+    return render(request,'componentsListPage.html')
+
+def createComponent(request):
+    return render(request )
+
+
+def updateComponent(request , pk):
+    return render(request)
+
+def updateComponent(request, pk):
+    return render(request)
